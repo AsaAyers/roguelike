fc93de98c33da1f2d57ce425d177e04c7efb1f1e
Asa Ayers <Asa@AsaAyers.com>
2019-04-06 15:01:52 -0700

    Bonus: Move render_bar and inventory_menu into Tcod::

--- src/main.rs
+++ src/main.rs
@@ -754,21 +754,56 @@ impl Tcod {
             None
         }
     }
-}
 
-fn inventory_menu(tcod: &mut Tcod, inventory: &[Object], header: &str) -> Option<usize> {
-    let options = if inventory.len() == 0 {
-        vec!["inventory is empty".into()]
-    } else {
-        inventory.iter().map(|item| item.name.clone()).collect()
-    };
+    pub fn render_bar(
+        self: &mut Tcod,
+        x: i32,
+        y: i32,
+        total_width: i32,
+        name: &str,
+        value: i32,
+        maximum: i32,
+        bar_color: Color,
+        back_color: Color,
+    ) {
+        // render a bar (HP, experience, etc).
+        let bar_width = (value as f32 / maximum as f32 * total_width as f32) as i32;
 
-    let inventory_index = tcod.menu(header, &options, INVENTORY_WIDTH);
+        //render the background first
+        self.panel.set_default_background(back_color);
+        self.panel
+            .rect(x, y, total_width, 1, false, BackgroundFlag::Screen);
 
-    if inventory.len() > 0 {
-        inventory_index
-    } else {
-        None
+        // now render the bar on top
+        self.panel.set_default_background(bar_color);
+        if bar_width > 0 {
+            self.panel
+                .rect(x, y, bar_width, 1, false, BackgroundFlag::Screen);
+        }
+
+        self.panel.set_default_foreground(colors::WHITE);
+        self.panel.print_ex(
+            x + total_width / 2,
+            y,
+            BackgroundFlag::None,
+            TextAlignment::Center,
+            &format!("{}: {}/{}", name, value, maximum),
+        )
+    }
+    pub fn inventory_menu(self: &mut Tcod, inventory: &[Object], header: &str) -> Option<usize> {
+        let options = if inventory.len() == 0 {
+            vec!["inventory is empty".into()]
+        } else {
+            inventory.iter().map(|item| item.name.clone()).collect()
+        };
+
+        let inventory_index = self.menu(header, &options, INVENTORY_WIDTH);
+
+        if inventory.len() > 0 {
+            inventory_index
+        } else {
+            None
+        }
     }
 }
 
@@ -829,8 +864,7 @@ fn handle_keys(
             TookTurn
         }
         (Key { printable: 'i', .. }, true) => {
-            let inventory_index = inventory_menu(
-                tcod,
+            let inventory_index = tcod.inventory_menu(
                 inventory,
                 "Press the key next to an item to use it, or any other to cancel.\n",
             );
@@ -864,40 +898,6 @@ fn handle_keys(
         (Key { code: Escape, .. }, _) => Exit,
         _ => DidntTakeTurn,
     }
-}
-
-fn render_bar(
-    panel: &mut Offscreen,
-    x: i32,
-    y: i32,
-    total_width: i32,
-    name: &str,
-    value: i32,
-    maximum: i32,
-    bar_color: Color,
-    back_color: Color,
-) {
-    // render a bar (HP, experience, etc).
-    let bar_width = (value as f32 / maximum as f32 * total_width as f32) as i32;
-
-    //render the background first
-    panel.set_default_background(back_color);
-    panel.rect(x, y, total_width, 1, false, BackgroundFlag::Screen);
-
-    // now render the bar on top
-    panel.set_default_background(bar_color);
-    if bar_width > 0 {
-        panel.rect(x, y, bar_width, 1, false, BackgroundFlag::Screen);
-    }
-
-    panel.set_default_foreground(colors::WHITE);
-    panel.print_ex(
-        x + total_width / 2,
-        y,
-        BackgroundFlag::None,
-        TextAlignment::Center,
-        &format!("{}: {}/{}", name, value, maximum),
-    )
 }
 
 fn render_all(
@@ -967,8 +967,7 @@ fn render_all(
     // show the player's stats
     let hp = objects[PLAYER].fighter.map_or(0, |f| f.hp);
     let max_hp = objects[PLAYER].fighter.map_or(0, |f| f.max_hp);
-    render_bar(
-        &mut tcod.panel,
+    tcod.render_bar(
         1,
         1,
         BAR_WIDTH,
